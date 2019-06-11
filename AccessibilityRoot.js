385d4e5efe74b1ea3923584b8a00a413782f5d62
Edele Gizatullin <me@edele.ru>
2019-06-11 14:25:25 +0500

    Add comment and custom focusable class

--- AccessibilityRoot.js
+++ AccessibilityRoot.js
@@ -1,32 +1,46 @@
 // @flow strict
 
-import * as React from 'react';
-import s from './AccessibilityRoot.m.less';
-import cn from 'classnames';
+import * as React from "react";
+import cn from "classnames";
+import s from "./AccessibilityRoot.m.less";
 
-type AccessibilityRootState = { userIsTabbing: boolean };
+/*
+This component disables outline on focus, until TAB button is pressed.
+
+By default it works only for <input>, <button> and so on.
+
+If you want your custom <div> to be focusable, you should
+import customFocusableClass like this:
+
+import { customFocusableClass } from '../../AccessibilityRoot';
+
+*/
+
+export const customFocusableClass = s.customFocusableClass;
+
+type AccessibilityRootState = {| userIsTabbing: boolean |};
 
 export default class AccessibilityRoot extends React.Component<{ children: React.Node }, AccessibilityRootState> {
-    state = { userIsTabbing: false };
+  state = { userIsTabbing: false };
 
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleFirstTab);
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleFirstTab);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleFirstTab);
+  }
+
+  handleFirstTab = (e: KeyboardEvent) => {
+    const ESC_KEY = 9;
+
+    if (e.keyCode === ESC_KEY) {
+      this.setState({ userIsTabbing: true });
+      window.removeEventListener("keydown", this.handleFirstTab);
     }
+  };
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleFirstTab);
-    }
-
-    handleFirstTab = (e: KeyboardEvent) => {
-        const ESC_KEY = 9;
-
-        if (e.keyCode === ESC_KEY) {
-            this.setState({ userIsTabbing: true });
-            window.removeEventListener('keydown', this.handleFirstTab);
-        }
-    };
-
-    render() {
-        return <div className={cn(s.root, this.state.userIsTabbing && s.userIsTabbing)}>{this.props.children}</div>;
-    }
+  render() {
+    return <div className={cn(s.root, this.state.userIsTabbing && s.userIsTabbing)}>{this.props.children}</div>;
+  }
 }
